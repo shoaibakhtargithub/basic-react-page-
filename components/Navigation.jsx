94323d22b0313c3f0b01f119cb0e1df85099fb24
shoaibakhtargithub <98732542+shoaibakhtargithub@users.noreply.github.com>
2024-08-12 00:13:47 +0530

    Add files via upload

---
+++ components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = ()=>{
+  return(
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li herf = "#">Menu</li>
+        <li herf = "#">Location</li>
+        <li herf = "#">About</li>
+        <li herf = "#">Contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+  );
+
+
+}
+export default Navigation;
